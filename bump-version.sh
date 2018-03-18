7f7dc70035db4f48df67735aca69946d95ade38e
Miguel Esteban Gutiérrez <miguel.esteban@extern.isban.com>
2018-03-18 23:20:03 +0100

    feat: Added option for showing current version

--- bump-version.sh
+++ bump-version.sh
@@ -13,6 +13,17 @@ function updateVersion() {
   sed -i "s/^\([ ]*\)var version='$1'$/\1var version='$2'/g" "$MAIN_FILE"
 }
 
+if [[ $# -eq 1 && $1 == "-c" ]]; then
+  echo "Current version is $(currentVersion)"
+  exit 0
+fi
+
+if [[ "$#" != "2" ]]; then
+  echo "Syntax: $(basename $0) <current-version> <new-version>"
+  echo "        $(basename $0) -c"
+  exit 1
+fi
+
 if hasVersion "$1"; then
   updateVersion "$1" "$2"
   echo "Files modified successfully, version bumped to $2."
